382b8e4dadf2d01cdf0213a27a30381daf67fd74
romanmandryk <roman.mandryk@gmail.com>
2023-01-26 18:44:35 +0000

    feat: initial blank implementation of svc-compliance

--- client-grpc/src/grpc.rs
+++ client-grpc/src/grpc.rs
@@ -1,29 +1,80 @@
+/// FlightPlanRequest
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct FlightPlanRequest {
+    /// Flight Plan Id
+    #[prost(string, tag = "1")]
+    pub flight_plan_id: ::prost::alloc::string::String,
+    /// JSON data of the flight plan
+    #[prost(string, tag = "2")]
+    pub data: ::prost::alloc::string::String,
+}
+/// FlightPlanResponse
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct FlightPlanResponse {
+    /// Flight Plan Id
+    #[prost(string, tag = "1")]
+    pub flight_plan_id: ::prost::alloc::string::String,
+    /// JSON data of the flight plan
+    #[prost(bool, tag = "2")]
+    pub submitted: bool,
+    /// Optional error or warning message
+    #[prost(string, optional, tag = "3")]
+    pub result: ::core::option::Option<::prost::alloc::string::String>,
+}
+/// FlightReleaseRequest
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct FlightReleaseRequest {
+    /// Flight Plan Id
+    #[prost(string, tag = "1")]
+    pub flight_plan_id: ::prost::alloc::string::String,
+    /// JSON data of the flight plan
+    #[prost(string, tag = "2")]
+    pub data: ::prost::alloc::string::String,
+}
+/// FlightReleaseResponse
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct FlightReleaseResponse {
+    /// Flight Plan Id
+    #[prost(string, tag = "1")]
+    pub flight_plan_id: ::prost::alloc::string::String,
+    /// JSON data of the flight plan
+    #[prost(bool, tag = "2")]
+    pub released: bool,
+    /// Optional error or warning message
+    #[prost(string, optional, tag = "3")]
+    pub result: ::core::option::Option<::prost::alloc::string::String>,
+}
 /// Are you Ready?
 ///
 /// No arguments
 #[derive(Eq, Copy)]
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct QueryIsReady {
-}
+pub struct QueryIsReady {}
 /// I'm Ready
 #[derive(Eq, Copy)]
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ReadyResponse {
     /// True if ready
-    #[prost(bool, tag="1")]
+    #[prost(bool, tag = "1")]
     pub ready: bool,
 }
 /// Generated client implementations.
-pub mod template_rust_rpc_client {
+pub mod compliance_rpc_client {
     #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
     use tonic::codegen::*;
     use tonic::codegen::http::Uri;
-    /// Heartbeat
+    /// ComplianceRpc
     #[derive(Debug, Clone)]
-    pub struct TemplateRustRpcClient<T> {
+    pub struct ComplianceRpcClient<T> {
         inner: tonic::client::Grpc<T>,
     }
-    impl TemplateRustRpcClient<tonic::transport::Channel> {
+    impl ComplianceRpcClient<tonic::transport::Channel> {
         /// Attempt to create a new client by connecting to a given endpoint.
         pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
         where
@@ -34,7 +85,7 @@ pub mod template_rust_rpc_client {
             Ok(Self::new(conn))
         }
     }
-    impl<T> TemplateRustRpcClient<T>
+    impl<T> ComplianceRpcClient<T>
     where
         T: tonic::client::GrpcService<tonic::body::BoxBody>,
         T::Error: Into<StdError>,
@@ -52,7 +103,7 @@ pub mod template_rust_rpc_client {
         pub fn with_interceptor<F>(
             inner: T,
             interceptor: F,
-        ) -> TemplateRustRpcClient<InterceptedService<T, F>>
+        ) -> ComplianceRpcClient<InterceptedService<T, F>>
         where
             F: tonic::service::Interceptor,
             T::ResponseBody: Default,
@@ -66,7 +117,7 @@ pub mod template_rust_rpc_client {
                 http::Request<tonic::body::BoxBody>,
             >>::Error: Into<StdError> + Send + Sync,
         {
-            TemplateRustRpcClient::new(InterceptedService::new(inner, interceptor))
+            ComplianceRpcClient::new(InterceptedService::new(inner, interceptor))
         }
         /// Compress requests with the given encoding.
         ///
@@ -83,7 +134,7 @@ pub mod template_rust_rpc_client {
             self.inner = self.inner.accept_compressed(encoding);
             self
         }
-        /// Common Interfaces
+        /// is ready heartbeat
         pub async fn is_ready(
             &mut self,
             request: impl tonic::IntoRequest<super::QueryIsReady>,
@@ -99,7 +150,47 @@ pub mod template_rust_rpc_client {
                 })?;
             let codec = tonic::codec::ProstCodec::default();
             let path = http::uri::PathAndQuery::from_static(
-                "/grpc.TemplateRustRpc/isReady",
+                "/grpc.ComplianceRpc/isReady",
+            );
+            self.inner.unary(request.into_request(), path, codec).await
+        }
+        /// submit flight plan
+        pub async fn submit_flight_plan(
+            &mut self,
+            request: impl tonic::IntoRequest<super::FlightPlanRequest>,
+        ) -> Result<tonic::Response<super::FlightPlanResponse>, tonic::Status> {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/grpc.ComplianceRpc/submitFlightPlan",
+            );
+            self.inner.unary(request.into_request(), path, codec).await
+        }
+        /// release flight plan
+        pub async fn request_flight_release(
+            &mut self,
+            request: impl tonic::IntoRequest<super::FlightReleaseRequest>,
+        ) -> Result<tonic::Response<super::FlightReleaseResponse>, tonic::Status> {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/grpc.ComplianceRpc/requestFlightRelease",
             );
             self.inner.unary(request.into_request(), path, codec).await
         }
---
+++ server/src/regions/mod.rs
@@ -0,0 +1,2 @@
+pub mod ne;
+pub mod us;
