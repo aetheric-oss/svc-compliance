65018b8e507d1d4749953de763a96baba11d1a80
owlot <95369289+owlot@users.noreply.github.com>
2023-05-23 15:45:51 +0200

    refactor(server): use lib-common traits and add mock features

--- server/src/region/macros.rs
+++ server/src/region/macros.rs
@@ -1,33 +1,4 @@
 //! log macro's for region logging
 
-/// Writes a debug! message to the app::region logger
-#[macro_export]
-macro_rules! region_debug {
-    ($($arg:tt)+) => {
-        log::debug!(target: "app::region", $($arg)+)
-    };
-}
-
-/// Writes an info! message to the app::region logger
-#[macro_export]
-macro_rules! region_info {
-    ($($arg:tt)+) => {
-        log::info!(target: "app::region", $($arg)+)
-    };
-}
-
-/// Writes an warn! message to the app::region logger
-#[macro_export]
-macro_rules! region_warn {
-    ($($arg:tt)+) => {
-        log::warn!(target: "app::region", $($arg)+)
-    };
-}
-
-/// Writes an error! message to the app::region logger
-#[macro_export]
-macro_rules! region_error {
-    ($($arg:tt)+) => {
-        log::error!(target: "app::region", $($arg)+)
-    };
-}
+use lib_common::log_macros;
+log_macros!("region");
