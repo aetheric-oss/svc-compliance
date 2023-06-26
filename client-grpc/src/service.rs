0d18f697f0aa16432151c2dd32d156f57acb84e8
owlot <95369289+owlot@users.noreply.github.com>
2023-06-26 21:38:38 +0200

    refactor: bring code in line with other services

--- client-grpc/src/service.rs
+++ client-grpc/src/service.rs
@@ -11,33 +11,188 @@ where
     /// The type expected for ReadyResponse structs.
     type ReadyResponse;
 
-    /// Wrapper for is_ready function.
+    /// Returns a [`tonic::Response`] containing a [`ReadyResponse`](Self::ReadyResponse)
+    /// Takes an [`ReadyRequest`](Self::ReadyRequest).
+    ///
+    /// # Errors
+    ///
+    /// Returns [`tonic::Status`] with [`Code::Unknown`](tonic::Code::Unknown) if
+    /// the server is not ready.
+    ///
+    /// # Examples
+    /// ```
+    /// use lib_common::grpc::get_endpoint_from_env;
+    /// use svc_compliance_client_grpc::client::{ReadyRequest, RpcServiceClient};
+    /// use svc_compliance_client_grpc::{Client, GrpcClient};
+    /// use svc_compliance_client_grpc::service::Client as ServiceClient;
+    /// use tonic::transport::Channel;
+    ///
+    /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+    ///     let (host, port) = get_endpoint_from_env("SERVER_HOSTNAME", "SERVER_PORT_GRPC");
+    ///     let connection = GrpcClient::<RpcServiceClient<Channel>>::new_client(&host, port, "compliance");
+    ///     let response = connection
+    ///         .is_ready(ReadyRequest {})
+    ///         .await?;
+    ///     println!("RESPONSE={:?}", response.into_inner());
+    ///     Ok(())
+    /// }
+    /// ```
     async fn is_ready(
         &self,
-        request: tonic::Request<Self::ReadyRequest>,
+        request: Self::ReadyRequest,
     ) -> Result<tonic::Response<Self::ReadyResponse>, tonic::Status>;
 
-    /// Wrapper for submit_flight_plan function
+    /// Returns a [`tonic::Response`] containing a [`FlightPlanResponse`](super::FlightPlanResponse)
+    /// Takes an [`FlightPlanRequest`](super::FlightPlanRequest).
+    ///
+    /// # Errors
+    ///
+    /// Returns [`tonic::Status`] with [`Code::Unknown`](tonic::Code::Unknown) if
+    /// the server is not ready.
+    ///
+    /// # Examples
+    /// ```
+    /// use lib_common::grpc::get_endpoint_from_env;
+    /// use svc_compliance_client_grpc::client::{FlightPlanRequest, RpcServiceClient};
+    /// use svc_compliance_client_grpc::{Client, GrpcClient};
+    /// use svc_compliance_client_grpc::service::Client as ServiceClient;
+    /// use tonic::transport::Channel;
+    ///
+    /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+    ///     let (host, port) = get_endpoint_from_env("SERVER_HOSTNAME", "SERVER_PORT_GRPC");
+    ///     let connection = GrpcClient::<RpcServiceClient<Channel>>::new_client(&host, port, "compliance");
+    ///     let response = connection
+    ///         .submit_flight_plan(FlightPlanRequest {
+    ///             flight_plan_id: "".to_string(),
+    ///             data: "".to_string(),
+    ///         })
+    ///         .await?;
+    ///     println!("submit_flight_plan RESPONSE={:?}", response.into_inner());
+    ///     Ok(())
+    /// }
+    /// ```
     async fn submit_flight_plan(
         &self,
-        request: tonic::Request<super::FlightPlanRequest>,
+        request: super::FlightPlanRequest,
     ) -> Result<tonic::Response<super::FlightPlanResponse>, tonic::Status>;
 
-    /// Wrapper for request_flight_release function
+    /// Returns a [`tonic::Response`] containing a [`FlightReleaseResponse`](super::FlightReleaseResponse)
+    /// Takes an [`FlightReleaseRequest`](super::FlightReleaseRequest).
+    ///
+    /// # Errors
+    ///
+    /// Returns [`tonic::Status`] with [`Code::Unknown`](tonic::Code::Unknown) if
+    /// the server is not ready.
+    ///
+    /// # Examples
+    /// ```
+    /// use lib_common::grpc::get_endpoint_from_env;
+    /// use svc_compliance_client_grpc::client::{FlightReleaseRequest, RpcServiceClient};
+    /// use svc_compliance_client_grpc::{Client, GrpcClient};
+    /// use svc_compliance_client_grpc::service::Client as ServiceClient;
+    /// use tonic::transport::Channel;
+    ///
+    /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+    ///     let (host, port) = get_endpoint_from_env("SERVER_HOSTNAME", "SERVER_PORT_GRPC");
+    ///     let connection = GrpcClient::<RpcServiceClient<Channel>>::new_client(&host, port, "compliance");
+    ///     let response = connection
+    ///         .request_flight_release(FlightReleaseRequest {
+    ///             flight_plan_id: "".to_string(),
+    ///             data: "".to_string(),
+    ///         })
+    ///         .await?;
+    ///     println!("submit_flight_plan RESPONSE={:?}", response.into_inner());
+    ///     Ok(())
+    /// }
+    /// ```
     async fn request_flight_release(
         &self,
-        request: tonic::Request<super::FlightReleaseRequest>,
+        request: super::FlightReleaseRequest,
     ) -> Result<tonic::Response<super::FlightReleaseResponse>, tonic::Status>;
 
-    /// Wrapper for request_waypoints function
+    /// Returns a [`tonic::Response`] containing a [`WaypointsResponse`](super::WaypointsResponse)
+    /// Takes an [`WaypointsRequest`](super::WaypointsRequest).
+    ///
+    /// # Errors
+    ///
+    /// Returns [`tonic::Status`] with [`Code::Unknown`](tonic::Code::Unknown) if
+    /// the server is not ready.
+    ///
+    /// # Examples
+    /// ```
+    /// use lib_common::grpc::get_endpoint_from_env;
+    /// use svc_compliance_client_grpc::client::{Coordinate, CoordinateFilter, WaypointsRequest, RpcServiceClient};
+    /// use svc_compliance_client_grpc::{Client, GrpcClient};
+    /// use svc_compliance_client_grpc::service::Client as ServiceClient;
+    /// use tonic::transport::Channel;
+    ///
+    /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+    ///     let (host, port) = get_endpoint_from_env("SERVER_HOSTNAME", "SERVER_PORT_GRPC");
+    ///     let connection = GrpcClient::<RpcServiceClient<Channel>>::new_client(&host, port, "compliance");
+    ///     let filter = CoordinateFilter {
+    ///         min: Some(Coordinate {
+    ///             latitude: 30.0,
+    ///             longitude: -105.0,
+    ///         }),
+    ///         max: Some(Coordinate {
+    ///             latitude: 35.0,
+    ///             longitude: -100.0,
+    ///         }),
+    ///     };
+    ///     let response = connection
+    ///         .request_waypoints(WaypointsRequest {
+    ///             filter: Some(filter)
+    ///         })
+    ///         .await?;
+    ///     println!("submit_flight_plan RESPONSE={:?}", response.into_inner());
+    ///     Ok(())
+    /// }
+    /// ```
     async fn request_waypoints(
         &self,
-        request: tonic::Request<super::WaypointsRequest>,
+        request: super::WaypointsRequest,
     ) -> Result<tonic::Response<super::WaypointsResponse>, tonic::Status>;
 
-    /// Wrapper for request_restrictions function
+    /// Returns a [`tonic::Response`] containing a [`RestrictionsResponse`](super::RestrictionsResponse)
+    /// Takes an [`RestrictionsRequest`](super::RestrictionsRequest).
+    ///
+    /// # Errors
+    ///
+    /// Returns [`tonic::Status`] with [`Code::Unknown`](tonic::Code::Unknown) if
+    /// the server is not ready.
+    ///
+    /// # Examples
+    /// ```
+    /// use lib_common::grpc::get_endpoint_from_env;
+    /// use svc_compliance_client_grpc::client::{Coordinate, CoordinateFilter, RestrictionsRequest, RpcServiceClient};
+    /// use svc_compliance_client_grpc::{Client, GrpcClient};
+    /// use svc_compliance_client_grpc::service::Client as ServiceClient;
+    /// use tonic::transport::Channel;
+    ///
+    /// async fn example () -> Result<(), Box<dyn std::error::Error>> {
+    ///     let (host, port) = get_endpoint_from_env("SERVER_HOSTNAME", "SERVER_PORT_GRPC");
+    ///     let connection = GrpcClient::<RpcServiceClient<Channel>>::new_client(&host, port, "compliance");
+    ///     let filter = CoordinateFilter {
+    ///         min: Some(Coordinate {
+    ///             latitude: 30.0,
+    ///             longitude: -105.0,
+    ///         }),
+    ///         max: Some(Coordinate {
+    ///             latitude: 35.0,
+    ///             longitude: -100.0,
+    ///         }),
+    ///     };
+    ///     let response = connection
+    ///         .request_restrictions(RestrictionsRequest {
+    ///             filter: Some(filter)
+    ///         })
+    ///         .await?;
+    ///     println!("submit_flight_plan RESPONSE={:?}", response.into_inner());
+    ///     Ok(())
+    /// }
+    /// ```
     async fn request_restrictions(
         &self,
-        request: tonic::Request<super::RestrictionsRequest>,
+        request: super::RestrictionsRequest,
     ) -> Result<tonic::Response<super::RestrictionsResponse>, tonic::Status>;
 }
--- server/tests/integration_test.rs
+++ server/tests/integration_test.rs
@@ -1,10 +1,27 @@
 //! Integration Tests
+//!
+fn get_log_string(function: &str, name: &str) -> String {
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "nl")] {
+            let lang = "nl";
+        } else {
+            let lang = "us";
+        }
+    }
+
+    #[cfg(feature = "stub_server")]
+    return format!("([{}] {} MOCK) {} server.", lang, function, name);
+
+    #[cfg(not(feature = "stub_server"))]
+    return format!("([{}] {}) {} server.", lang, function, name);
+}
 
 #[tokio::test]
 async fn test_server_requests_and_logs() {
     use logtest::Logger;
-
     use svc_compliance::grpc::server::*;
+
+    let name = "compliance";
 
     // Start the logger.
     let mut logger = Logger::start();
@@ -17,15 +34,17 @@ async fn test_server_requests_and_logs() {
         let result: ReadyResponse = result.unwrap().into_inner();
         assert_eq!(result.ready, true);
 
+        // Search for the expected log message
+        let expected = get_log_string("is_ready", name);
+        println!("expected message: {}", expected);
         assert!(logger.any(|log| {
-            let message = log.args();
-            println!("{:?}", message);
-            cfg_if::cfg_if! {
-                if #[cfg(feature = "nl")] {
-                    log.args() == format!("([nl] is_ready MOCK) {} server.", "compliance")
-                } else {
-                    log.args() == format!("([us] is_ready MOCK) {} server.", "compliance")
-                }
+            if log.target().contains("app::") {
+                println!("{}", log.target());
+                let message = log.args();
+                println!("{:?}", message);
+                log.args() == expected
+            } else {
+                false
             }
         }));
     }
